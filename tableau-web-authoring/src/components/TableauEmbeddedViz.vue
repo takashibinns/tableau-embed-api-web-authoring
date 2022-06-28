2f7affda38d78b8d8da97a0d4be72f52c6cad9fe
Takashi Binns <takashi.binns@icloud.com>
2022-06-28 16:30:54 -0400

    initial working version

---
+++ tableau-web-authoring/src/components/TableauEmbeddedViz.vue
@@ -0,0 +1,16 @@
+<template>
+    <tableau-authoring-viz v-if="authoringMode" id="TableauViz2" :src="vizUrl"></tableau-authoring-viz>
+    <tableau-viz v-else id="TableauViz1" :src="vizUrl" toolbar="hidden" hide-tabs></tableau-viz>
+</template>
+
+<script>
+export default {
+    name: 'TableauEmbeddedViz',
+    props: {
+        vizUrl: String,
+        authoringMode: Boolean
+    }
+}
+</script>
+
+<style scoped></style>
